87f2c98e2f9375aa82bd605014983086526a0c60
cah-nathanielanneken <annekent@miamioh.edu>
2016-03-11 09:54:03 -0500

    Final Functionality Implemented
    App should be fully functional.  Items left include UI changes, and
    admin privileges once server access has been granted

--- Dynasoft/db/seeds.rb
+++ Dynasoft/db/seeds.rb
@@ -1,28 +1,28 @@
 User.create!(name: "annekent", admin: true)
 
-Roombase.create!(name: "Shade Family Room", location: "test",
-                   maxOccupancy: 100)
-Roombase.create!(name: "Dining Commons/Galleria", location: "test",
+Roombase.create!(name: "Shade Family Room", location: "1st Floor",
+                   maxOccupancy: 150)
+Roombase.create!(name: "Dining Commons/Galleria", location: "1st Floor",
+                 maxOccupancy: 600)
+Roombase.create!(name: "Wiikiaami", location: "2nd Floor",
+                 maxOccupancy: 25)
+Roombase.create!(name: "Commuter Resource Center", location: "3rd Floor",
+                 maxOccupancy: 20)
+Roombase.create!(name: "Community Lounge", location: "2nd Floor",
+                 maxOccupancy: 50)
+Roombase.create!(name: "SEAL(2nd Floor)", location: "2nd Floor",
                  maxOccupancy: 100)
-Roombase.create!(name: "Reflection Room", location: "test",
+Roombase.create!(name: "Meeting/Quiet Study Rooms(2nd Floor)", location: "2nd Floor",
+                 maxOccupancy: 80)
+Roombase.create!(name: "SEAL(3rd Floor)", location: "3rd Floor",
                  maxOccupancy: 100)
-Roombase.create!(name: "Wiikiaami", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Commuter Resource Center", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Community Lounge", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "SEAL(2nd Floor)", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Meeting/Quiet Study Rooms(2nd Floor)", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "SEAL(3rd Floor)", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Meeting/Quiet Study Rooms(3rd Floor)", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Computer Lab/Printing Areas", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Pulley Dinner", location: "test",
-                 maxOccupancy: 100)
-Roombase.create!(name: "Cafaro Lounge", location: "test",
-                 maxOccupancy: 100)
+Roombase.create!(name: "Meeting/Quiet Study Rooms(3rd Floor)", location: "3rd Floor",
+                 maxOccupancy: 60)
+Roombase.create!(name: "Computer Lab/Printing Areas", location: "3rd Floor",
+                 maxOccupancy: 15)
+Roombase.create!(name: "Pulley Dinner", location: "2nd Floor",
+                 maxOccupancy: 250)
+Roombase.create!(name: "Cafaro Lounge", location: "3rd Floor",
+                 maxOccupancy: 500)
+Roombase.create!(name: "Ford Reflection Room", location: "2nd Floor",
+                 maxOccupancy: 25)
